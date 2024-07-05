b3d015d5bf37f73d89a88b0968197d88a62e2aab
mikael-v <mikael-v@outlook.com>
2024-07-05 15:10:06 +0100

    adds buttons to add game to lists

--- screens/OneGame.tsx
+++ screens/OneGame.tsx
@@ -9,9 +9,18 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
+import {
+  AnimatedFAB,
+  Provider,
+  Portal,
+  Modal,
+  Button,
+} from "react-native-paper";
 
-const OneGame = ({ route }) => {
+const OneGame = ({ route, visible, animateFrom }) => {
+  const [isExtended, setIsExtended] = useState(false);
   const [rating, setRating] = useState(0);
+  const [optionsVisible, setOptionsVisible] = useState(false);
   const { game } = route.params;
 
   const handlePlatform = (url) => {
@@ -24,32 +33,86 @@ const OneGame = ({ route }) => {
     setRating(rating);
   };
 
+  const openOptions = () => {
+    setIsExtended(!isExtended);
+    setOptionsVisible(!optionsVisible);
+  };
+
+  const closeOptions = () => {
+    setIsExtended(false);
+    setOptionsVisible(false);
+  };
+
   return (
-    <ScrollView style={styles.container}>
-      <Text style={styles.title}>{game.title}</Text>
-      <Image source={game.img} style={styles.image} />
-      <View style={styles.ratingContainer}>
-        <Rating
-          showRating={false}
-          onFinishRating={onRatingCompleted}
-          tintColor="#0a0a31"
-        />
-      </View>
-      <Text style={styles.description}>{game.description}</Text>
-      <Text style={styles.genres}>Genres: {game.genres.join(", ")}</Text>
-      <View style={styles.platformContainer}>
-        <Text style={styles.availableOn}>Available On: </Text>
-        {game.platforms.map((eachPlatform, index) => (
-          <TouchableOpacity
-            key={index}
-            onPress={() => handlePlatform(eachPlatform.url)}
-            style={styles.button}
-          >
-            <Text style={styles.buttonText}>{eachPlatform.name} </Text>
-          </TouchableOpacity>
-        ))}
-      </View>
-    </ScrollView>
+    <Provider>
+      <ScrollView style={styles.container}>
+        <Text style={styles.title}>{game.title}</Text>
+        <Image source={game.img} style={styles.image} />
+        <View style={styles.ratingContainer}>
+          <Rating
+            showRating={false}
+            onFinishRating={onRatingCompleted}
+            tintColor="#0a0a31"
+          />
+        </View>
+        <Text style={styles.description}>{game.description}</Text>
+        <Text style={styles.genres}>Genres: {game.genres.join(", ")}</Text>
+        <View style={styles.platformContainer}>
+          <Text style={styles.availableOn}>Available On: </Text>
+          {game.platforms.map((eachPlatform, index) => (
+            <TouchableOpacity
+              key={index}
+              onPress={() => handlePlatform(eachPlatform.url)}
+              style={styles.button}
+            >
+              <Text style={styles.buttonText}>{eachPlatform.name} </Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </ScrollView>
+      <Portal>
+        <Modal
+          visible={optionsVisible}
+          onDismiss={closeOptions}
+          contentContainerStyle={styles.modalContent}
+        >
+          <View>
+            <Button
+              icon="plus"
+              mode="contained"
+              onPress={() => {
+                console.log("Add to My Games");
+                closeOptions();
+              }}
+              style={styles.modalButton}
+            >
+              Add to My Games
+            </Button>
+            <Button
+              icon="plus"
+              mode="contained"
+              onPress={() => {
+                console.log("Add to Wishlist");
+                closeOptions();
+              }}
+              style={styles.modalButton}
+            >
+              Add to Wishlist
+            </Button>
+          </View>
+        </Modal>
+      </Portal>
+      <AnimatedFAB
+        icon="plus"
+        label="Add to Collection"
+        extended={true}
+        onPress={openOptions}
+        visible={visible}
+        animateFrom={"right"}
+        iconMode="static"
+        style={styles.fabStyle}
+      />
+    </Provider>
   );
 };
 
@@ -112,7 +175,25 @@ const styles = StyleSheet.create({
   },
   ratingContainer: {
     marginTop: 15,
-    
+  },
+  fabStyle: {
+    position: "absolute",
+    bottom: 90,
+    justifyContent: "center",
+    alignSelf: "center",
+    marginHorizontal: 10,
+    padding: 8,
+    backgroundColor: "#f20089",
+  },
+  modalContent: {
+    backgroundColor: "#3f4386",
+    padding: 20,
+    marginHorizontal: 20,
+    borderRadius: 15,
+  },
+  modalButton: {
+    marginVertical: 5,
+    backgroundColor: "#f20089",
   },
 });
 
