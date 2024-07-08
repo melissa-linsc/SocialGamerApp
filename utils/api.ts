4c9d2ab5fa9e9121397390dcda0061814c771b8f
mikael-v <mikael-v@outlook.com>
2024-07-08 17:21:49 +0100

    completed refactoring preferences to get data from backend api

--- utils/api.ts
+++ utils/api.ts
@@ -1,4 +1,30 @@
-const axios = require('axios');
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-const gamerly = axios.create({ baseURL: "https://us-central1-debugd1vas.cloudfunctions.net/app/api" })
+const gamerly = axios.create({
+  baseURL: "https://us-central1-debugd1vas.cloudfunctions.net/app/api",
+});
 
+//Get all games sorted by rating
+export const fetchGames = () => {
+  return gamerly
+    .get("/games?sortField=rating&sortOrder=desc")
+    .then((response) => {
+      return response.data.games;
+    })
+    .catch((error) => {
+      console.error("Error fetching games:", error);
+    });
+};
+
+//Get all genres
+export const fetchGenres = () => {
+  return gamerly
+    .get("/genres")
+    .then((response) => {
+      return response.data.genres;
+    })
+    .catch((error) => {
+      console.error("Error fetching genres:", error);
+    });
+};
