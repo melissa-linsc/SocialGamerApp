5000815a54d81a34cfef724ce96c3135b461bf5c
melissa-linsc <118235468+melissa-linsc@users.noreply.github.com>
2024-07-08 16:53:34 +0100

    sends search result to game page when clicked

--- screens/OneGame.tsx
+++ screens/OneGame.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Linking } from "react-native";
 import { Rating } from "react-native-ratings";
 import {
@@ -16,12 +16,28 @@ import {
   Modal,
   Button,
 } from "react-native-paper";
+import GradientText from "react-native-gradient-texts";
+import { getGameById } from "../utils/api";
+import { formatGameTitle, formatDescription } from "../utils/utils";
 
 const OneGame = ({ route, visible, animateFrom }) => {
+
+  const [gameData, setGameData] = useState({})
+  const [isLoading, setIsLoading] = useState(true)
+
   const [isExtended, setIsExtended] = useState(false);
   const [rating, setRating] = useState(0);
   const [optionsVisible, setOptionsVisible] = useState(false);
-  const { game } = route.params;
+  const { gameid } = route.params;
+
+  useEffect(() => {
+    getGameById(gameid).then((result) => {
+      setGameData(result)
+      console.log(Object.keys(result))
+      console.log(result.genres)
+      setIsLoading(false)
+    })
+  }, [])
 
   const handlePlatform = (url) => {
     Linking.openURL(url).catch((err) =>
@@ -43,36 +59,99 @@ const OneGame = ({ route, visible, animateFrom }) => {
     setOptionsVisible(false);
   };
 
+  if (isLoading) {
+    return <View>
+      <Text>Loading...</Text>
+    </View>
+  }
+
   return (
     <Provider>
-      <ScrollView style={styles.container}>
-        <Text style={styles.title}>{game.title}</Text>
-        <Image source={game.img} style={styles.image} />
-        <View style={styles.ratingContainer}>
-          <Rating
-            showRating={false}
-            onFinishRating={onRatingCompleted}
-            tintColor="#0a0a31"
-            ratingBackgroundColor="#00ff15"
-            ratingColor="#ff0000"
-      
-            // style={{ borderWidth: 0, padding: 2, borderColor: "#00ff15" }}
-          />
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.imageContainer}>
+          <Image source={{uri: gameData.background_image}} style={styles.image} />
         </View>
-        <Text style={styles.description}>{game.description}</Text>
-        <Text style={styles.genres}>Genres: {game.genres.join(", ")}</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>
+              {formatGameTitle(gameData.slug)}
+            </Text>
+        <View style={styles.allRatings}>
+            {gameData.ratings.map((rating) => {
+              if (rating.title === 'recommended') {
+                return <View style={styles.gameStat}>
+                <Text style={styles.buttonText}>{rating.title}</Text>
+                <Text style={styles.buttonText}>{rating.percent}%</Text>
+                </View> 
+              }
+            })}
+            <View style={styles.gameStat}>
+                <Text style={styles.buttonText}>Released</Text>
+                <Text style={styles.buttonText}>{gameData.released}</Text>
+            </View>
+            {gameData.esrb_rating ? <View style={styles.gameStat}>
+                <Text style={styles.buttonText}>ESRB</Text>
+                <Text style={styles.buttonText}>{gameData.esrb_rating.name}</Text>
+            </View> : null}
+        </View>
+        <Text style={styles.description}>{formatDescription(gameData.description)}</Text>
+        {/* <Text style={styles.genres}>Genres: {gameData.genres.join(", ")}</Text> */}
         <View style={styles.platformContainer}>
           <Text style={styles.availableOn}>Available On: </Text>
-          {game.platforms.map((eachPlatform, index) => (
+          {gameData.platforms.map((platforms) => (
             <TouchableOpacity
-              key={index}
-              onPress={() => handlePlatform(eachPlatform.url)}
+              key={platforms.platform.id}
+              // onPress={() => handlePlatform(platform.url)}
               style={styles.button}
             >
-              <Text style={styles.buttonText}>{eachPlatform.name} </Text>
+              <Text style={styles.buttonText}>{platforms.platform.name} </Text>
             </TouchableOpacity>
           ))}
         </View>
+        <View style={styles.platformContainer}>
+          <Text style={styles.availableOn}>Genres: </Text>
+          {gameData.genres.map((genre) => (
+              <TouchableOpacity
+                key={genre.id}
+                // onPress={() => handlePlatform(platform.url)}
+                style={styles.genreButton}
+              >
+                <Text style={styles.genreButtonText}>{genre.slug}</Text>
+              </TouchableOpacity>
+            ))}
+        </View>
+        <View style={styles.platformContainer}>
+          <Text style={styles.availableOn}>Tags: </Text>
+          {/* {gameData.genres.map((genre) => (
+              <TouchableOpacity
+                key={genre.id}
+                // onPress={() => handlePlatform(platform.url)}
+                style={styles.genreButton}
+              >
+                <Text style={styles.buttonText}>{genre.slug}</Text>
+              </TouchableOpacity>
+            ))} */}
+          {gameData.tags.map((tag) => (
+              <TouchableOpacity
+                key={tag.id}
+                // onPress={() => handlePlatform(platform.url)}
+                style={styles.button}
+              >
+                <Text style={styles.buttonText}>{tag.slug}</Text>
+              </TouchableOpacity>
+            ))}
+        </View>
+          <View style={styles.ratingContainer}>
+            <Rating
+              showRating={false}
+              onFinishRating={onRatingCompleted}
+              tintColor="#0a0a31"
+              ratingBackgroundColor="#00ff15"
+              ratingColor="#ff0000"
+        
+              // style={{ borderWidth: 0, padding: 2, borderColor: "#00ff15" }}
+            />
+          </View>
+          </View>
       </ScrollView>
       <Portal>
         <Modal
@@ -126,18 +205,56 @@ const styles = StyleSheet.create({
     backgroundColor: "#0a0a31",
     paddingHorizontal: 10,
   },
+  scrollView: {
+    backgroundColor: "#0a0a31",
+  },
+  titleContainer: {
+    width: 420,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  allRatings: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  gameStat: {
+    backgroundColor: "#4cc9f0",
+    padding: 0,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    padding: 10,
+    margin: 5
+  },
   image: {
-    width: "75%",
+    width: "100%",
+    backgroundColor: "#0a0a31",
     height: 300,
     marginLeft: "auto",
     marginRight: "auto",
+    borderBottomEndRadius: 15,
+    borderBottomStartRadius: 15,
+   
+  },
+  imageContainer: {
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.7,
+    shadowRadius: 2,
+    elevation: 5, // For Android
+    paddingBottom: 10,
   },
   title: {
     color: "#fff",
-    fontSize: 24,
+    fontSize: 30,
     fontWeight: "bold",
     textAlign: "center",
     marginVertical: 20,
+    textShadowColor: '#f20089', // Set the shadow color
+    textShadowOffset: { width: 0, height: 0 }, // Set the shadow offset
+    textShadowRadius: 20, // Set the shadow radius
   },
   genres: {
     color: "#fff",
@@ -153,6 +270,9 @@ const styles = StyleSheet.create({
     alignContent: "center",
     marginLeft: "auto",
     marginRight: "auto",
+    borderTopWidth: 2,
+    borderBottomWidth: 2,
+    borderColor: "#fff",
   },
   platformContainer: {
     flexDirection: "row",
@@ -160,7 +280,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     marginTop: 20,
     flexWrap: "wrap",
-    marginBottom: 150,
   },
   availableOn: {
     color: "#fff",
@@ -174,12 +293,24 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     alignItems: "center",
   },
+  genreButton: {
+    backgroundColor: "#4cc9f0",
+    padding: 5,
+    margin: 5,
+    borderRadius: 5,
+    alignItems: "center",
+  },
+  genreButtonText: {
+    color: "#0a0a31",
+    fontSize: 15,
+  },
   buttonText: {
     color: "#fff",
     fontSize: 15,
   },
   ratingContainer: {
-    marginTop: 15,
+    marginTop: 25,
+    marginBottom: 150,
     // outline: 'none'
   },
   fabStyle: {
@@ -189,7 +320,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
     padding: 8,
     paddingLeft: 0,
-    backgroundColor: "#f20089",
+    backgroundColor: "#4cc9f0",
   },
   modalContent: {
     backgroundColor: "#0a0a31",
--- utils/api.ts
+++ utils/api.ts
@@ -1,4 +1,27 @@
-const axios = require('axios');
+import axios from 'axios'
 
 const gamerly = axios.create({ baseURL: "https://us-central1-debugd1vas.cloudfunctions.net/app/api" })
 
+export function getSearchedGames(searchQuery:string): Promise<any[]> {
+    return gamerly.get('/games', 
+        {
+            params: {
+                search: searchQuery,
+                limit: 5,
+            }
+        }
+    ).then((searchData) => {
+        return searchData.data.games
+    })
+    .catch((err) => {
+        console.log("Error Fetching Games")
+        throw Error
+    })
+}
+
+export function getGameById(gameid) {
+     return gamerly.get(`/games/${gameid}`).then((gameData) => {
+        console.log(gameData.data)
+        return gameData.data.gameById
+     })
+}
